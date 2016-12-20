de32c900efe6763456d0f201dbdfe87c30ebeac6
Brian Wong <brian.wongst@gmail.com>
2016-12-20 21:56:07 +0800

    Add capstrano production.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,15 @@
+`ssh-add`
 # config valid only for current version of Capistrano
 lock "3.7.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "forum"
+set :repo_url, "git@github.com:brianwst/forum.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/deploy/forum"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -21,13 +22,15 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
+set :passenger_restart_with_touch, true
+
